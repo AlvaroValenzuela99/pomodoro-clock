e6e64bf0bd03464ce0db565e5a1986d765a8b053
Alvaro Valenzuela <alvarovgerena@gmail.com>
2023-08-24 10:09:55 +0200

    created logic to change break length and session length managing the status of the components

--- src/components/BreakLength.js
+++ src/components/BreakLength.js
@@ -3,18 +3,37 @@ import React, { Component } from 'react';
 class BreakLength extends Component {
     constructor(props){
         super(props);
-        this.setState({
+        this.state = {
             breakLength: 5
-        })
+        }
+
+        this.decrementBreakLength = this.decrementBreakLength.bind(this);
+        this.incrementBreakLength = this.incrementBreakLength.bind(this);
     }
+
+    decrementBreakLength(){
+        if(this.state.breakLength > 0){
+            this.setState({
+                breakLength: this.state.breakLength - 1
+            })
+        }
+    }
+    incrementBreakLength(){
+        if(this.state.breakLength < 60){
+            this.setState({
+                breakLength: this.state.breakLength + 1
+            })
+        }
+    }
+
     render() {
         return (
             <div id="break-label">
                 <h2>Break Length</h2>
                 <div className="control">
-                    <i id="break-decrement" class="fa-solid fa-arrow-down"></i>
-                    <p id="break-length">5</p>
-                    <i id="break-increment" class="fa-solid fa-arrow-up"></i>
+                    <i id="break-decrement" class="fa-solid fa-arrow-down" onClick={this.decrementBreakLength}></i>
+                    <p id="break-length">{this.state.breakLength}</p>
+                    <i id="break-increment" class="fa-solid fa-arrow-up" onClick={this.incrementBreakLength}></i>
                 </div>
             </div>
         );
--- src/components/SessionLength.js
+++ src/components/SessionLength.js
@@ -3,18 +3,37 @@ import React, { Component } from 'react';
 class SessionLength extends Component {
     constructor(props){
         super(props)
-        this.setState({
+        this.state = {
             sessionLength: 25
-        })
+        }
+
+        this.decrementSessionLength = this.decrementSessionLength.bind(this);
+        this.incrementSessionLength = this.incrementSessionLength.bind(this);
     }
+
+    decrementSessionLength(){
+        if(this.state.sessionLength > 0){
+            this.setState({
+                sessionLength: this.state.sessionLength - 1
+            })
+        }
+    }
+    incrementSessionLength(){
+        if(this.state.sessionLength < 60){
+            this.setState({
+                sessionLength: this.state.sessionLength + 1
+            })
+        }
+    }
+
     render() {
         return (
             <div id="session-label">
                 <h2>Session Length</h2>
                 <div className="control">
-                    <i id="session-decrement" class="fa-solid fa-arrow-down"></i>
-                    <p id="session-length">25</p>
-                    <i id="session-increment" class="fa-solid fa-arrow-up"></i>
+                    <i id="session-decrement" class="fa-solid fa-arrow-down" onClick={this.decrementSessionLength}></i>
+                    <p id="session-length">{this.state.sessionLength}</p>
+                    <i id="session-increment" class="fa-solid fa-arrow-up" onClick={this.incrementSessionLength}></i>
                 </div>
             </div>
         );
